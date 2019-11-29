5b3b450cf3e1e9e731a756e381d1c5f8568b22f2
Simon Sorensen <teamangryghast@gmail.com>
2019-11-29 12:40:32 +0000

    Testing out some ideas

--- valkyrie.cpp
+++ valkyrie.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <list>
 
+using namespace std;
+
+class Node {
+    public:
+        bool selfClosing;
+        string openingTag;
+        string closingTag;
+        list<Node> children;
+};
 
 int main() {
-    std::cout << "Valkyrie 1.0\n";
+    cout << "Valkyrie 1.0" << endl;
+
+    Node test;
+    test.openingTag = "<html>";
+    test.closingTag = "</html>";
+
+
+
+    test.children.push_back(test);
+
+    cout << test.openingTag << endl;
+    cout << test.children.front().closingTag << endl;
 
     return 0;
 }
